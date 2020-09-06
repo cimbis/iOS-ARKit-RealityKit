0acf2fbdaf1a96a99f70031a4c4c4bc26bbb5463
JC <cimbulis.j@gmail.com>
2020-09-06 16:34:48 +0300

    delete

--- ImageDetectionAR/ImageDetectionAR/ViewController.swift
+++ ImageDetectionAR/ImageDetectionAR/ViewController.swift
@@ -60,8 +60,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
 
     // MARK: - ARSCNViewDelegate
-
-    // Override to create and configure nodes for anchors added to the view's session.
     func renderer(
             _ renderer: SCNSceneRenderer,
             nodeFor anchor: ARAnchor
